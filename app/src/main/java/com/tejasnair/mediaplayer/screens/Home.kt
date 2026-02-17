d3211eb52979c97e36d00fa2107aaac91d81cf54
tnair3 <nairstejas@gmail.com>
2026-02-17 17:57:34 +0000

    17.02.25 Working towards a Home Screen

---
+++ app/src/main/java/com/tejasnair/mediaplayer/screens/Home.kt
@@ -0,0 +1,60 @@
+package com.tejasnair.mediaplayer.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.tejasnair.mediaplayer.ui.theme.MediaPlayerTheme
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Welcome to my app")
+            ClickableButton()
+        }
+    }
+}
+
+@Composable
+fun ClickableButton() {
+    var buttonText by remember { mutableStateOf("Meow") }
+    var clickNum by remember { mutableIntStateOf(0) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                if(!buttonText.equals("Clicked")) {
+                    buttonText = "Clicked"
+                }
+                clickNum++
+            }
+        ) {
+            if(clickNum > 0) {
+                Text("$buttonText $clickNum")
+            }
+            else {
+                Text(buttonText)
+            }
+        }
+    }
+}
