e50dc0c3863475233cbe3632e55f19c7eef017f6
tnair3 <nairstejas@gmail.com>
2026-04-13 15:52:18 +0100

    Fix for Issue#14

--- app/src/main/java/com/tejasnair/mediaplayer/ui/components/FormatValues.kt
+++ app/src/main/java/com/tejasnair/mediaplayer/ui/components/FormatValues.kt
@@ -19,6 +19,6 @@ fun formatBytes(bytes: Long): String {
 fun formatTime(ms: Long): String {
     val totalSeconds = ms / 1000
     val minutes = totalSeconds / 60
-    val seconds = totalSeconds % 60
+    val seconds = if(totalSeconds > 0) totalSeconds % 60 else 0
     return "%02d:%02d".format(minutes, seconds)
 }
