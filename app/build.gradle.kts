b3407767e41caa2acde7fe37b0603d256c87789e
tnair3 <nairstejas@gmail.com>
2026-04-21 19:04:11 +0100

    App version update to 1.0-alpha2 ahead of release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0-alpha1"
+        versionName = "1.0-alpha2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
